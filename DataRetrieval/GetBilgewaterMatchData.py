a68791e16a61cff8144fce270f3471b1909367fb
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-11 18:30:07 +0100

    getmatch data updated

--- DataRetrieval/GetBilgewaterMatchData.py
+++ DataRetrieval/GetBilgewaterMatchData.py
@@ -3,21 +3,26 @@ __author__ = 'Kishan'
 import requests
 import os
 import json
-import urllib
 import sys
+import time
 
-api_key = ''
-
-https = 'https://'
-riot_api = '.api.pvp.net/api/lol/'
-match_api = '/v2.2/match/'
-timeline = 'true'
 rootpath = os.path.split(os.getcwd())[0]
-noError = 3
+error = 1
 regions = []
 matchIds = []
+matchIdDirectory = 'MatchIDs/BilgewaterMatchIDs'
+matchDataDirectory = 'BilgewaterMatchData'
 
-def urlBuilder(region, matchId, timeline, api_key):
+matchDataFolder = r'E:\Google Drive\RiotAPIChallenge2.0\MatchData'
+
+
+
+def urlBuilder(region, matchId, api_key):
+    https = 'https://'
+    riot_api = '.api.pvp.net/api/lol/'
+    match_api = '/v2.2/match/'
+    timeline = 'true'
+
     url = https + region + riot_api + region + match_api + matchId + '?includeTimeline=' + timeline + '&api_key=' + api_key
     return url
 
@@ -26,7 +31,7 @@ def getMatchIds():
     matchRegionsJson = []
     matchIds.clear()
     regions.clear()
-    path = os.path.join(rootpath, 'MatchData\MatchIDs\BilgewaterMatchIDs')
+    path = os.path.join(matchDataFolder, matchIdDirectory)
 
     for f in os.listdir(path):
         if os.path.isfile(os.path.join(path, f.title())):
@@ -44,14 +49,17 @@ def getMatchIds():
 
 
 
-def getMatchData(matchId, region):
-    url = urlBuilder(region, matchId, timeline, api_key)
-    path = os.path.join(rootpath, 'MatchData\BilgewaterMatchData', region.upper(), matchId + '.json')
+def getMatchData(matchId, region, api_key):
+    url = urlBuilder(region, matchId, api_key)
+    path = os.path.join(matchDataFolder, matchDataDirectory, region.upper(), matchId + '.json')
     if (os.path.isfile(path)):
         return 0
     data = requests.get(url).json()
+    time.sleep(0.5)
 
-    if (len(data) <= noError):
+    if (len(data) == error):
+        print()
+        print(data)
         return -1
 
 
@@ -62,20 +70,19 @@ def getMatchData(matchId, region):
 
 
 def main():
-    global api_key
+
     api_key = input('Enter you api-key: ')
     matchIds = getMatchIds()
 
-    progressCounter = 0
+    progressCounter = 10000
     for i in range(len(matchIds)):
         for j in range(len(matchIds[i])):
-            status = getMatchData(str(matchIds[i][j]), regions[i])
+            status = getMatchData(str(matchIds[i][j]), regions[i], api_key)
             if (status == -1):
                 print('\nAPI Error')
                 return
-            progressCounter += 1
-            progress = str(10000*i+j+1)
-            sys.stdout.write('\rProgress: ' + progress)
+            progressCounter -= 1
+            sys.stdout.write('\rProgress Countdown: ' + regions[i].upper() + ' ' +  str(progressCounter))
             sys.stdout.flush()
 
 
