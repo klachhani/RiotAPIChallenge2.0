c501f56faf8740c57a5a5848542b62dc0d170a26
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-12 15:51:54 +0100

    read api keys from json file in projects root directory

--- DataRetrieval/matchdata/GetMatchData.py
+++ DataRetrieval/matchdata/GetMatchData.py
@@ -1,5 +1,7 @@
 __author__ = 'Kishan'
 
+
+import dataretrieval.matchdata
 import requests
 import os
 import json
@@ -7,36 +9,55 @@ import sys
 from pushbullet import PushBullet
 import time
 
-rootpath = os.path.split(os.getcwd())[0]
+riot_api_key = dataretrieval.matchdata.riot_api_key
+pb_api_key = dataretrieval.matchdata.pushbullet_api_key
+matchDataFolder = r'E:\Google Drive\RiotAPIChallenge2.0\MatchData'
+
 error = 1
 regions = []
 matchIds = []
 matchIdDirectory = 'MatchIDs/BilgewaterMatchIDs'
 matchDataDirectory = 'BilgewaterMatchData'
 apiMaxRetryAttempts = 15
-riot_api_key = ''
-pb_api_key = ''
-progressThisSesson = int
+progressThisSession = int
 start = time
 end = time
 
-matchDataFolder = r'E:\Google Drive\RiotAPIChallenge2.0\MatchData'
+
+def main():
+
+    global progressThisSession
+    global start
+
+    start = time.clock()
+    progressThisSession = 0
+    match_ids = get_match_ids()
+
+    for i in range(len(match_ids)):
+        progress_counter = len(match_ids[i])
+        for j in range(len(match_ids[i])):
+            status = get_match_data(str(match_ids[i][j]), regions[i], progress_counter)
+            if status == -1:
+                return
+            progress_counter -= 1
+            sys.stdout.write('\rProgress Countdown: ' + regions[i].upper() +
+                             ' ' +  str(progress_counter))
+            sys.stdout.flush()
 
 
-
-def urlBuilder(region, matchId, riot_api_key):
+def url_builder(region, match_id, riot_api_key):
     https = 'https://'
     riot_api = '.api.pvp.net/api/lol/'
     match_api = '/v2.2/match/'
     timeline = 'true'
 
-    url = https + region + riot_api + region + match_api + matchId + \
+    url = https + region + riot_api + region + match_api + match_id + \
           '?includeTimeline=' + timeline + '&api_key=' + riot_api_key
     return url
 
 
-def getMatchIds():
-    matchRegionsJson = []
+def get_match_ids():
+    match_regions_json = []
     matchIds.clear()
     regions.clear()
     path = os.path.join(matchDataFolder, matchIdDirectory)
@@ -44,93 +65,67 @@ def getMatchIds():
     for f in os.listdir(path):
         if f.endswith('.json'):
             if os.path.isfile(os.path.join(path, f.title())):
-                matchRegionsJson.append(os.path.join(path, f))
+                match_regions_json.append(os.path.join(path, f))
 
     for f in os.listdir(path):
         if os.path.isfile(os.path.join(path, f.title())):
             regions.append((f[:-5]).lower())
 
-    for r in matchRegionsJson:
+    for r in match_regions_json:
         f = open(r)
         matchIds.append(json.load(f))
 
     return matchIds
 
 
+def get_match_data(match_id, region, progress_counter):
 
-def getMatchData(matchId, region, progressCounter):
     global end
-    global progressThisSesson
-    url = urlBuilder(region, matchId, riot_api_key)
-    path = os.path.join(matchDataFolder, matchDataDirectory, region.upper(), matchId + '.json')
-    if (os.path.isfile(path)):
+    global progressThisSession
+    url = url_builder(region, match_id, riot_api_key)
+    path = os.path.join(matchDataFolder, matchDataDirectory, region.upper(), match_id + '.json')
+    if os.path.isfile(path):
         return 0
 
     status_code_not_ok = bool
     retry_counter = 0
     while status_code_not_ok:
-        data = requests.request('GET', url)
+        try:
+            data = requests.request('GET', url)
+        except ConnectionError:
+            retry_counter += 1
+            print('ConnectionError')
+            continue
         retry_counter += 1
-        if data.ok == False:
+        if not data.ok:
             status_code_not_ok = True
             sys.stdout.write('\rProgress Countdown: ' + region.upper() + ' ' +
-                             str(progressCounter) + '\t\t' + 'Http status code: ' +
+                             str(progress_counter) + '\t\t' + 'Http status code: ' +
                              str(data.status_code) + '\tRetry attempt...' +
                              str(retry_counter) + '/' + str(apiMaxRetryAttempts))
             sys.stdout.flush()
             if retry_counter == apiMaxRetryAttempts:
                 end = time.clock()
-                thisSessionMesg = 'This session: ' + str(progressThisSesson) + ' Matches retrieved in %.2f' % (end-start) + ' seconds'
+                this_session_Message = 'This session: ' + str(progressThisSession) + ' Matches retrieved in %.2f' % (end-start) + ' seconds'
                 if pb_api_key != '':
                     pb = PushBullet(pb_api_key)
                     pb.push_note('Riot API Data Retrieval', 'Data Retrieval Aborted, ' +
-                                 str(progressCounter) + ' remaining.\n' + thisSessionMesg)
+                                 str(progress_counter) + ' remaining for ' + region.upper()
+                                 + '.\n' + this_session_Message)
 
                 print( '...Aborting')
-                print(thisSessionMesg)
+                print(this_session_Message)
                 return -1
         else:
             data = data.json()
             status_code_not_ok = False
-            if retry_counter > 1:
-                print()
 
 
     #time.sleep(0.8)
 
     with open(path, 'w') as outfile:
-        progressThisSesson += 1
+        progressThisSession += 1
         json.dump(data, outfile)
-
-
-def main():
-
-    global riot_api_key
-    global pb_api_key
-    global progressThisSesson
-    global start
-    riot_api_key = input('Enter your Riot api-key: ')
-    pb_api_key = input('Enter your Pushbullet api-key (optional): ')
-
-    start = time.clock()
-    progressThisSesson = 0
-    matchIds = getMatchIds()
-
-
-    for i in range(len(matchIds)):
-        progressCounter = len(matchIds[i])
-        for j in range(len(matchIds[i])):
-            status = getMatchData(str(matchIds[i][j]), regions[i], progressCounter)
-            if (status == -1):
-                return
-            progressCounter -= 1
-            sys.stdout.write('\rProgress Countdown: ' + regions[i].upper() +
-                             ' ' +  str(progressCounter))
-            sys.stdout.flush()
-
-
-
-
 
 
 if __name__ == '__main__':
