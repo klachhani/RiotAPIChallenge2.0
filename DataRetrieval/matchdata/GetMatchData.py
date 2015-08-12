ffef0308e38104f79d729b5c10541e024df85130
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-12 13:47:20 +0100

    console and pushbullet messages updated

--- DataRetrieval/matchdata/GetMatchData.py
+++ DataRetrieval/matchdata/GetMatchData.py
@@ -16,6 +16,9 @@ matchDataDirectory = 'BilgewaterMatchData'
 apiMaxRetryAttempts = 15
 riot_api_key = ''
 pb_api_key = ''
+progressThisSesson = int
+start = time
+end = time
 
 matchDataFolder = r'E:\Google Drive\RiotAPIChallenge2.0\MatchData'
 
@@ -27,7 +30,8 @@ def urlBuilder(region, matchId, riot_api_key):
     match_api = '/v2.2/match/'
     timeline = 'true'
 
-    url = https + region + riot_api + region + match_api + matchId + '?includeTimeline=' + timeline + '&api_key=' + riot_api_key
+    url = https + region + riot_api + region + match_api + matchId + \
+          '?includeTimeline=' + timeline + '&api_key=' + riot_api_key
     return url
 
 
@@ -55,6 +59,8 @@ def getMatchIds():
 
 
 def getMatchData(matchId, region, progressCounter):
+    global end
+    global progressThisSesson
     url = urlBuilder(region, matchId, riot_api_key)
     path = os.path.join(matchDataFolder, matchDataDirectory, region.upper(), matchId + '.json')
     if (os.path.isfile(path)):
@@ -73,10 +79,15 @@ def getMatchData(matchId, region, progressCounter):
                              str(retry_counter) + '/' + str(apiMaxRetryAttempts))
             sys.stdout.flush()
             if retry_counter == apiMaxRetryAttempts:
+                end = time.clock()
+                thisSessionMesg = 'This session: ' + str(progressThisSesson) + ' Matches retrieved in %.2f' % (end-start) + ' seconds'
                 if pb_api_key != '':
                     pb = PushBullet(pb_api_key)
-                    pb.push_note('Riot API Data Retrieval', 'Data Retrieval Aborted, ' + str(progressCounter) + ' remaining')
+                    pb.push_note('Riot API Data Retrieval', 'Data Retrieval Aborted, ' +
+                                 str(progressCounter) + ' remaining.\n' + thisSessionMesg)
+
                 print( '...Aborting')
+                print(thisSessionMesg)
                 return -1
         else:
             data = data.json()
@@ -88,6 +99,7 @@ def getMatchData(matchId, region, progressCounter):
     #time.sleep(0.8)
 
     with open(path, 'w') as outfile:
+        progressThisSesson += 1
         json.dump(data, outfile)
 
 
@@ -95,8 +107,13 @@ def main():
 
     global riot_api_key
     global pb_api_key
+    global progressThisSesson
+    global start
     riot_api_key = input('Enter your Riot api-key: ')
     pb_api_key = input('Enter your Pushbullet api-key (optional): ')
+
+    start = time.clock()
+    progressThisSesson = 0
     matchIds = getMatchIds()
 
 
@@ -107,7 +124,8 @@ def main():
             if (status == -1):
                 return
             progressCounter -= 1
-            sys.stdout.write('\rProgress Countdown: ' + regions[i].upper() + ' ' +  str(progressCounter))
+            sys.stdout.write('\rProgress Countdown: ' + regions[i].upper() +
+                             ' ' +  str(progressCounter))
             sys.stdout.flush()
 
 
