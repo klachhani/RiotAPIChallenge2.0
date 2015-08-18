d4ab6cb2128893e1f469ec676ba0469b2def0a8f
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-18 21:07:01 +0100

    updated url_requests method

--- data_retrieval/url_requests.py
+++ data_retrieval/url_requests.py
@@ -3,17 +3,9 @@ __author__ = 'Kishan'
 import requests
 import sys
 from socket import gaierror
-from pushbullet import Pushbullet
-import config.config as config
+import time
 
-pushbullet_api_key = config.pushbullet_api_key
-
-if not pushbullet_api_key == '':
-    pb = Pushbullet(pushbullet_api_key)
-
-
-
-def request(url, max_attempts, region, progress_counter = -1):
+def request(url, max_attempts, region = '', progress_counter = -1):
     statuscode_not_ok = True
     attempts = 0
 
@@ -22,8 +14,6 @@ def request(url, max_attempts, region, progress_counter = -1):
             data = requests.get(url)
         except gaierror:
             print('\n')
-            if not pushbullet_api_key == '':
-                pb.push_note('Connection error', gaierror)
             sys.exit(gaierror)
         if data.ok:
             return data.json()
@@ -33,15 +23,12 @@ def request(url, max_attempts, region, progress_counter = -1):
             if not progress_counter == -1:
                 progress_countdown_error(region, progress_counter,
                                          data, attempts, max_attempts)
+            else:
+                progress_countdown_error('', '', data, attempts, max_attempts)
+                sys.stdout.flush()
         if attempts == max_attempts:
-            print('\n')
-            err_msg = 'Error: ' + str(data.status_code) + ': ' \
-                      + data.json()['status']['message']
-            if not pushbullet_api_key == '':
-                pb.push_note('Reached max attempts', err_msg + '\n'
-                             + progress_counter + ' Remaning for '
-                             + region.upper())
-            sys.exit('Error: '
+            time.sleep(1)
+            sys.exit('\nError: '
                      + str(data.status_code) + ': '
                      + data.json()['status']['message'])
 
@@ -51,3 +38,4 @@ def progress_countdown_error(region, progress_counter, data, attempts, max_attem
                                  str(progress_counter) + '\t\t' + 'Http status code: ' +
                                  str(data.status_code) + '\tRetry attempt...' +
                                  str(attempts) + '/' + str(max_attempts))
+    sys.stdout.flush()
