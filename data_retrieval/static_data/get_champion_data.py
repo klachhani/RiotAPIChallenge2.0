ba1adc92652719d97d41e8fc88f7bec270dba10f
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-18 21:05:27 +0100

    get champion data method added

--- data_retrieval/static_data/get_champion_data.py
+++ data_retrieval/static_data/get_champion_data.py
@@ -1,15 +1,18 @@
 __author__ = 'Kishan'
 
-import requests
-import config.config as config
-import data_retrieval.url_requests as url_requests
+from config import config
+from data_retrieval import url_requests
+from  data_retrieval import static_data
+
+max_attempts = static_data.max_attempts
+
 
 def get_champion_by_id(region):
     url = 'https://global.api.pvp.net/api/lol/static-data/'\
           + region + '/v1.2/champion?locale=en_US&champData=all&api_key='\
           + config.riot_api_key
 
-    champ_data = url_requests.request(url, 15)
+    champ_data = url_requests.request(url, max_attempts)
     return champ_data['keys']
 
 
