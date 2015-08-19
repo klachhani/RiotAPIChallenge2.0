d1ee1bdb7721b792e1c199850a869b4a02a10656
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-19 16:25:35 +0100

    fixed imports

--- data_analytics/data_aggregation/champions.py
+++ data_analytics/data_aggregation/champions.py
@@ -3,12 +3,13 @@ __author__ = 'Kishan'
 from data_retrieval.match_data import get_match_data
 from data_retrieval import static_data
 from data_retrieval.static_data import get_champion_data
+from data_analytics import data_aggregation
 from config import config
 import json
 import os
 import sys
 
-regions = [(get_match_data.get_match_regions())[0]] #Limit regions?
+regions = [(get_match_data.get_match_regions())[0]] #Only 'br'
 
 print('\nCHAMPIONS\n')
 
@@ -30,59 +31,40 @@ def main():
 
     sys.stdout.write('Writing champions JSON...')
     sys.stdout.flush()
-    write_champions_json(champions_json)
+    data_aggregation.write_json(champions_json, 'champions.json')
     sys.stdout.write('done!')
     sys.stdout.flush()
 
 
-def create_champions_dict(champions_json):
+def create_champions_dict(dict):
     for r in regions:
-        champions_json[r] = {}
+        dict[r] = {}
         champion_keys = get_champion_data.get_champion_by_id(r)
         for hast in static_data.highest_achieved_season_tier:
-            champions_json[r][hast] = {}
+            dict[r][hast] = {}
             for key, value in champion_keys.items():
-                champions_json[r][hast][key] = {'key': value, 'won': 0, 'lost': 0}
+                dict[r][hast][key] = {'key': value, 'won': 0, 'lost': 0}
 
 
-def populate_dict(champions_json):
+def populate_dict(dict):
     for r in regions:
         match_data_directory = os.path.join(config.match_data_directory, r.upper())
         matches = os.listdir(match_data_directory)
         progress_counter = len(matches)
         for m in matches:
-            if not m.endswith('json'):
-                continue
-            else:
-                match_data = os.path.join(match_data_directory, m)
-                with open(match_data, 'r') as f: #open match file
-                    data = json.load(f) #load match file as json
-                    winner = 0
-                    for t in range(2):
-                        if data['teams'][t]['winner'] == True:
-                            winner = data['teams'][t]['teamId']
-                    for p in range(10):
-                        tier = data['participants'][p]['highestAchievedSeasonTier']
-                        champion_id = data['participants'][p]['championId']
-                        team_id = data['participants'][p]['teamId']
-                        if winner == team_id:
-                            champions_json['br'][tier][str(champion_id)]['won'] += 1
-                        else:
-                            champions_json['br'][tier][str(champion_id)]['lost'] += 1
+            if not m.endswith('json'): continue
+            match_data = os.path.join(match_data_directory, m)
+            with open(match_data, 'r') as f: #open match file
+                data = json.load(f) #load match file as json
+                win_team_id = [t['teamId'] for t in data['teams'] if t['winner']][0]
+                for p in data['participants']:
+                    tier = p['highestAchievedSeasonTier']
+                    champion_id = p['championId']
+                    team_id = p['teamId']
+                    if win_team_id == team_id: dict[r][tier][str(champion_id)]['won'] += 1
+                    else: dict[r][tier][str(champion_id)]['lost'] += 1
             progress_counter -= 1
-            progress_countdown(progress_counter, r)
-
-
-def write_champions_json(champions_json):
-    os.chdir(os.path.dirname(__file__))
-    with open('champions.json', 'w') as outfile:
-        json.dump(champions_json, outfile)
-
-
-def progress_countdown(progress_counter, region):
-    sys.stdout.write('\rPopulating dict...' + region.upper() +
-                             ' ' +  str(progress_counter))
-    sys.stdout.flush()
+            data_aggregation.progress_countdown(progress_counter, r)
 
 
 if __name__ == '__main__':
