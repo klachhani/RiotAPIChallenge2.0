1501943a0f55b6f46eb4ae9c4dc8101b5060eaf4
Kishan Lachhani <kishan.lachhani@gmail.com>
2015-08-25 18:01:00 +0100

    updated js and d3 for champions

--- FlaskApp/FlaskApp/static/js/datacharts.js
+++ FlaskApp/FlaskApp/static/js/datacharts.js
@@ -15,28 +15,26 @@ function getQueryURL(regions, tiers, category, results_url){
     return results_url + "?" + query;
 }
 
-function champPickRate(regions, tiers, results_url) {
+function champChart(regions, tiers, results_url, mode) {
 
     query_url = getQueryURL(regions, tiers, 'champions', results_url);
 
-    initialiseChampPickrate();
-
-}
-
-function initialiseChampPickrate(){
-
     $.getJSON(query_url, function(data){
-        data = data.pickrate;
-        console.log(data);
-
+        if (mode == 'pickrate'){
+            data = data.pickrate;
+        } else {
+            data = data.winrate;
+        }
+        data = data.slice(0,19);
         var max = 0
+
         for (i in data){
             max = Math.max(max, data[i][2])
         }
 
         var width = $('#chart-container').width();
-        var barHeight = 35;
-        var startwidth = 150;
+        var barHeight = 50;
+        var startwidth = 50;
 
         var x = d3.scale.linear()
             .domain([0, max])
@@ -52,7 +50,14 @@ function initialiseChampPickrate(){
 
         bars.attr("height", barHeight - 1)
             .transition()
-            .duration(3000)
+            .duration(1000)
+            .attr("width", 0);
+
+
+        bars.attr("height", barHeight - 1)
+            .transition()
+            .delay(1000)
+            .duration(1000)
             .attr("width", function(d) { return x(d[2]);});
 
         bars.enter().append("rect")
@@ -61,47 +66,60 @@ function initialiseChampPickrate(){
             .attr("width", 0)
             .attr("height", barHeight - 1)
             .transition()
-            .duration(3000)
+            .duration(1000)
             .attr("width", function(d) { return x(d[2]);});
 
 
         bars.exit().select("rect").remove();
-/*
-        //update champ icon
-        var bars = chart.selectAll("svg:image").data(data);
 
-        bars.attr("height", barHeight - 1)
+        //update champ icon
+        var img = chart.selectAll("image").data(data);
+
+        img.attr("width", barHeight - 3)
+            .transition()
+            .delay(1000)
             .attr("xlink:href", function(d){return "http://ddragon.leagueoflegends.com/cdn/5.15.1/img/champion/" + d[1] + ".png";});
 
-        bars.enter().append("svg:image")
+        img.enter().append("image")
             .attr("transform", function(d, i) {return "translate(" + (startwidth-barHeight) + "," + i * barHeight + ")";})
             .attr("xlink:href", function(d){return "http://ddragon.leagueoflegends.com/cdn/5.15.1/img/champion/" + d[1] + ".png";})
-            .attr("width", barHeight)
-            .attr("height", barHeight);
+            .attr("width", barHeight - 3)
+            .attr("height", barHeight - 3);
 
-        bars.exit().select("svg:image").remove();*/
+        img.exit().select("image").remove();
 
-    //update champ text
-        var bars = chart.selectAll("text").data(data);
+        //update champ text
+        var text = chart.selectAll("text").data(data);
 
-        bars.attr("height", barHeight - 1)
+        text.attr("height", barHeight)
             .transition()
-            .text(function(d){return d[1] + '-' + d[2] + '-' + d[3]});
+            .delay(1000)
+            .attr("transform", function(d, i) {return "translate(" + (x(d[2]) + startwidth/2) + "," + i * barHeight + ")";})
+            .text(function(d){
+                    if (mode == 'pickrate'){
+                            return d[1] + '      ' + d[2] + '%      ' + d[3];
+                        } else {
+                            return d[1] + '      ' + d[2] + '%';
+                        }
+
+            });
 
 
-        bars.enter().append("text")
-            .transition()
-            .attr("transform", function(d, i) {return "translate(" + (0) + "," + i * barHeight + ")";})
-            .attr("dy", barHeight/2)
-            .text(function(d){return d[1] + '-' + d[2] + '-' + d[3]});
+        text.enter().append("text")
+            .attr("transform", function(d, i) {return "translate(" + (x(d[2]) + startwidth/2) + "," + i * barHeight + ")";})
+            .attr("dy", barHeight/2 + 5)
+            .text(function(d){
+                    if (mode == 'pickrate'){
+                            return d[1] + '      ' + d[2] + '%      ' + d[3];
+                        } else {
+                            return d[1] + '      ' + d[2] + '%';
+                        }
+                });
 
-        bars.exit().select("text").remove();
+        text.exit().select("text").remove();
 
     });
 }
-
-
-
 
 
 
